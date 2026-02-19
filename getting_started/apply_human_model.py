f3af97ea28f8fd8b7a07fa09301e358332c73a14
Balding Donkey <s1053436@student.knoxschools.org>
2026-02-19 21:30:00 +0000

    slight changes

--- getting_started/apply_human_model.py
+++ getting_started/apply_human_model.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from getting_started.human_classifier import human_classify
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('/workspaces/Human-vs-ML-Project/data/hawks_new.csv')
-sampled_df = df.sample(frac=0.7, random_state=32)
+training_df,test_df = train_test_split( df, test_size=0.3, random_state=32)
+
